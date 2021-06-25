93dede4a027e87991b5e0fc3cc842d0654dc5753
josephjang <jyj3@illinois.edu>
2021-06-25 00:53:19 -0700

    Dielectric Material

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ int main() {
     // Image
 
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 1000;
+    const int image_width = 1920;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
@@ -43,19 +43,20 @@ int main() {
     hittable_list world;
     
     auto material_ground = make_shared<lambertian>(color(0.7, 0.67, 0.6));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+    auto material_center = make_shared<dielectric>(1.5);
     auto material_center_left = make_shared<metal>(color(0.55, 0.95, 1.0), 0.0);
     auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.0);
     auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.7);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0, -0.2, -1.0), 0.3, material_center));
-    world.add(make_shared<sphere>(point3( -0.3, -0.4, -0.8), 0.1, material_center_left));
-    world.add(make_shared<sphere>(point3(-0.8, 0.0, -1.1), 0.5, material_left));
-    world.add(make_shared<sphere>(point3( 0.8, 0.2, -1.4), 0.7, material_right));
+    world.add(make_shared<sphere>(point3( 0.0, -0.2, -1.0), -0.2, material_center));
+    world.add(make_shared<sphere>(point3( -0.3, -0.4, -0.75), 0.1, material_center_left));
+    world.add(make_shared<sphere>(point3(-0.9, 0.0, -1.3), 0.5, material_left));
+    world.add(make_shared<sphere>(point3( 0.85, 0.2, -1.6), 0.7, material_right));
 
     // Camera
-    camera cam;
+    camera cam(point3(0,0,0), point3(0,0,-1), vec3(0,1,0), 90, aspect_ratio);
 
     // Render
 
